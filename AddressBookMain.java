558215411c991253cd4f616f42ba825e919c3e3b
unknown <kumar.skm03@gmail.com>
2021-02-24 20:17:13 +0530

    added method addContact in AddressBookMain.java

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -2,12 +2,13 @@
 import java.util.*;
 
 class Contact {
+	//variables
 	private String firstName, lastName;
 	private String address, city, state;
 	private String zip;
 	private String phoneNum;
 	private String email;
-
+	//intiallize variables by constructor
 	public Contact(String firstName, String lastName, String address, String city, String state, String zip, String phoneNum, String email) {
         	System.out.println("Welcome to Address Book Program");
 		this.firstName = firstName;
@@ -27,10 +28,40 @@ class Contact {
 }
 
 public class AddressBookMain {
+	//to add new objects
+	static ArrayList<Contact> list = new ArrayList<Contact>();
+	//taking input from user
+	public static void addContact() {
+		String firstName, lastName, address, city, state, zip, phoneNum, email;
+		Scanner sc = new Scanner(System.in);
+		String check = "y";
+		while(check == "y" || check == "Y") {
+			System.out.print("Enter First Name: ");
+			firstName = sc.nextLine();
+			System.out.print("Enter Last Name: ");
+                	lastName = sc.nextLine();
+			System.out.print("Enter Address: ");
+                	address = sc.nextLine();
+			System.out.print("Enter City: ");
+                	city = sc.nextLine();
+			System.out.print("Enter State ");
+                	state = sc.nextLine();
+			System.out.print("Enter ZIP Code: ");
+                	zip = sc.nextLine();
+			System.out.print("Enter Phone Number: ");
+                	phoneNum = sc.nextLine();
+			System.out.print("Enter Email: ");
+                	email = sc.nextLine();
+			System.out.print("Do you want to add more Contacts(y/n): ");
+			check = sc.nextLine();
+			System.out.println();
+
+			list.add( new Contact(firstName, lastName, address, city, state, zip, phoneNum, email));
+		}
+	}
 
 	public static void main(String[] args) {
-		ArrayList<Contact> list = new ArrayList<Contact>();
-		list.add( new Contact("Shubham", "Kumar", "B-138, NFL Vijaipur Township", "Guna", "Madhya Pradesh", "473111", "9111649077", "kumar.skm03@gmailcom"));
+		addContact();
 		System.out.println(list.get(0));
 	}
 }

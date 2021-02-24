8c120f3e497c4e18f270d37fbab531390ac854c8
unknown <kumar.skm03@gmail.com>
2021-02-24 12:44:33 +0530

    added AddressBookMain.java

---
+++ AddressBookMain.java
@@ -0,0 +1,12 @@
+
+class AddAddress {
+    public AddAddress() {
+        System.out.println("Welcome to Address Book Program");
+    }
+}
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        AddAddress contact = new AddAddress();
+    }
+}

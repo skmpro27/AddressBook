cb7a1e7dd737c8371977fea3e7b0f0054d6a2c32
unknown <kumar.skm03@gmail.com>
2021-02-24 16:37:22 +0530

    added a constuctor and fuction to add contact in AddressBookMain.java

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,12 +1,36 @@
 
-class AddAddress {
-    public AddAddress() {
-        System.out.println("Welcome to Address Book Program");
-    }
+import java.util.*;
+
+class Contact {
+	private String firstName, lastName;
+	private String address, city, state;
+	private String zip;
+	private String phoneNum;
+	private String email;
+
+	public Contact(String firstName, String lastName, String address, String city, String state, String zip, String phoneNum, String email) {
+        	System.out.println("Welcome to Address Book Program");
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNum = phoneNum;
+		this.email = email;
+    	}
+	public String toString() {
+		return "Name: " + firstName + " " + lastName +
+		       "\nAddress: " + address + ", " + city + ", " + state +
+		       "\nZIP: " + zip + "\nPhone Number: " + phoneNum + "\nEmail: " + email;
+	}
 }
 
 public class AddressBookMain {
-    public static void main(String[] args) {
-        AddAddress contact = new AddAddress();
-    }
+
+	public static void main(String[] args) {
+		ArrayList<Contact> list = new ArrayList<Contact>();
+		list.add( new Contact("Shubham", "Kumar", "B-138, NFL Vijaipur Township", "Guna", "Madhya Pradesh", "473111", "9111649077", "kumar.skm03@gmailcom"));
+		System.out.println(list.get(0));
+	}
 }

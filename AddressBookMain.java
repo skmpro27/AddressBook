10a7517b051c338d551d45c7e65e43ff6f850691
unknown <kumar.skm03@gmail.com>
2021-02-24 21:45:29 +0530

    added edit method to edit contact in AddressBookMain.java

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,14 +3,13 @@ import java.util.*;
 
 class Contact {
 	//variables
-	private String firstName, lastName;
-	private String address, city, state;
-	private String zip;
-	private String phoneNum;
-	private String email;
+	public String firstName, lastName;
+	public String address, city, state;
+	public String zip;
+	public String phoneNum;
+	public String email;
 	//intiallize variables by constructor
 	public Contact(String firstName, String lastName, String address, String city, String state, String zip, String phoneNum, String email) {
-        	System.out.println("Welcome to Address Book Program");
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.address = address;
@@ -30,12 +29,15 @@ class Contact {
 public class AddressBookMain {
 	//to add new objects
 	static ArrayList<Contact> list = new ArrayList<Contact>();
+
+	static Scanner sc = new Scanner(System.in);
+
 	//taking input from user
 	public static void addContact() {
 		String firstName, lastName, address, city, state, zip, phoneNum, email;
 		Scanner sc = new Scanner(System.in);
 		String check = "y";
-		while(check == "y" || check == "Y") {
+		while(check.equals("y") || check.equals("Y")) {
 			System.out.print("Enter First Name: ");
 			firstName = sc.nextLine();
 			System.out.print("Enter Last Name: ");
@@ -60,8 +62,42 @@ public class AddressBookMain {
 		}
 	}
 
+	public static String edit() {
+		String name1st, name2nd;
+		System.out.print("Enter first name: ");
+		name1st = sc.nextLine();
+		System.out.print("Enter Second name: ");
+		name2nd = sc.nextLine();
+
+		for(int i = 0; i < list.size(); i++) {
+			if (name1st.equals(list.get(i).firstName) && name2nd.equals(list.get(i).lastName)) {
+				System.out.print("Enter First Name: ");
+				list.get(i).firstName = sc.nextLine();
+				System.out.print("Enter Last Name: ");
+                		list.get(i).lastName = sc.nextLine();
+				System.out.print("Enter Address: ");
+                		list.get(i).address = sc.nextLine();
+				System.out.print("Enter City: ");
+        	        	list.get(i).city = sc.nextLine();
+				System.out.print("Enter State ");
+                		list.get(i).state = sc.nextLine();
+				System.out.print("Enter ZIP Code: ");
+        	        	list.get(i).zip = sc.nextLine();
+				System.out.print("Enter Phone Number: ");
+                		list.get(i).phoneNum = sc.nextLine();
+				System.out.print("Enter Email: ");
+	                	list.get(i).email = sc.nextLine();
+				System.out.println();
+				System.out.println(list.get(i));
+				return "Updated";
+			}
+		}
+		return "Name not found";
+	}
+
 	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
 		addContact();
-		System.out.println(list.get(0));
+		System.out.println(edit());
 	}
 }
